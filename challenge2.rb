884d8ce22b545570c1393d8ac558692afd9da579
Simon Cass <scass1991@gmail.com>
2018-07-23 19:53:17 +0100

    initial commit

---
+++ challenge2.rb
@@ -0,0 +1,10 @@
+#July 23rd 2018
+#Just like with functions in other languages, Ruby methods can have arguments
+
+puts("learn to code").include?("code")
+
+#this returns true in IRB (puts not needed there)
+
+puts("learn to code").include?("CODE")
+
+#returns false - include? is case sensitive
