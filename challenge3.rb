07f9a46e85231f75605bc0a181b1531d1a9bce6f
Simon Cass <scass1991@gmail.com>
2018-07-23 20:07:23 +0100

    initial commit

---
+++ challenge3.rb
@@ -0,0 +1,11 @@
+#July 23rd 2018
+
+Bank_balance = 1010
+
+def dinner_plans
+    if Bank_balance > 100
+        return "You should go out for dinner tonight!"
+    else
+        return "Stay at home"
+    end
+end
