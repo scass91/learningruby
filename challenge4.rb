19da2fd5f29618c9c847882725f9a0e0b58f12ad
Simon Cass <scass1991@gmail.com>
2018-07-23 20:17:26 +0100

    initial commit

---
+++ challenge4.rb
@@ -0,0 +1,10 @@
+#July 23rd 2018
+#Write out a method that whill let you say happy birthday to someone 3 times
+
+Name = "dave"
+
+def happy_birthday_to(name)
+    puts("Happy Birthday #{name}")
+    puts("Happy Birthday #{name}")
+    puts("Happy Birthday #{name}")
+end
