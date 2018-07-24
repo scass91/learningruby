32ebc136094c7d5386dd849f474bc23ccd5899c3
Simon Cass <scass1991@gmail.com>
2018-07-24 19:31:45 +0100

    initial commit

---
+++ bonus3.rb
@@ -0,0 +1,9 @@
+#July 24th 2018
+#Write a method called 'shout' that takes a String as an input and returns the
+#String in capital letters - again return doesn't work in Atom
+
+def shout(randomstring)
+    return(randomstring.upcase)
+end
+
+shout("hello")
