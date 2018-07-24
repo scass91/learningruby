aee0e1b74c9a7aa6d298c62a4ba922e1106383e6
Simon Cass <scass1991@gmail.com>
2018-07-24 18:29:15 +0100

    initial commit

---
+++ challenge7.rb
@@ -0,0 +1,5 @@
+#July 24th 2018
+#A hash is an Array where items are accessed by a unique key instead of a number
+
+fruits = {"banana" => "yellow", "apple" => "red", "orange" => "orange"}
+puts(fruits["banana"])
