b5cbf8e0746331807ce72c753a97f8c0cbea9694
Simon Cass <scass1991@gmail.com>
2018-07-24 18:38:41 +0100

    initial commit

---
+++ challenge8.rb
@@ -0,0 +1,9 @@
+#July 24th 2018
+#can you complete the beow code to complete the rhyme?
+
+default = "clap your hands"
+special = "and you really want to show it"
+
+[default, default, special, default].each do |x|
+    puts "If you're happy and you know it #{x}"
+end
