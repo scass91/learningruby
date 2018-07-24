1032816f74b79dc18b917e7a69fa23176e40be97
Simon Cass <scass1991@gmail.com>
2018-07-24 19:44:02 +0100

    initial commit

---
+++ bonus4.rb
@@ -0,0 +1,9 @@
+#July 24th 2018
+#Write a greeter method which takes a name as an input such that I could write
+#greeter("Jordan") and it would display "Hello Jordan, how are you today?"
+
+def greeter(name)
+    puts("Hello #{name}! How are you today?")
+end
+
+greeter("Jordan")
