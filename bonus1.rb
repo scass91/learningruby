e71ad16782c75d4909e676efaf068183349355c7
Simon Cass <scass1991@gmail.com>
2018-07-24 19:16:22 +0100

    initial commit

---
+++ bonus1.rb
@@ -0,0 +1,12 @@
+#July 24th 2018
+#Write a method which will tell you if a number is odd or even
+
+def evenorodd(number)
+    if number % 2 == 0
+        puts "That is an even number"
+    else
+        puts "That is an odd number"
+    end
+end
+
+evenorodd(2)
