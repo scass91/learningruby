cab2cb8df8de07807e6895cd001a5db2be6cffa7
Simon Cass <scass1991@gmail.com>
2018-07-24 19:11:13 +0100

    initial commit

---
+++ capital_cities.rb
@@ -0,0 +1,9 @@
+#July 24th 2018
+
+capital_cities = {"England"=>"London", "Spain"=>"Madrid","Japan"=>"Tokyo",
+    "Ireland"=>"Dublin", "France"=>"Paris", "Ukraine"=>"Kiev",
+    "Latvia"=>"Riga", "Thailand"=>"Bangkok", "Poland"=>"Warsaw",
+    "Sweden"=>"Stockholm", "Denmark"=>"Copenhagen", "Turkey"=>"Ankara",
+    "Netherlands"=>"Amsterdam", "Portugal"=>"Lisbon", "Wales"=>"Cardiff"}
+
+capital_cities.each{|country, city| puts "#{country}'s capital is #{city}"}
