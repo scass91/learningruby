375b545522a4be3eebeb8397c87073e00e175409
Simon Cass <scass1991@gmail.com>
2018-07-24 19:03:33 +0100

    initial commit

---
+++ challenge9.rb
@@ -0,0 +1,6 @@
+#July 24th 2018
+#Create an Array of numbers and see if you can iterate over them, displaying,
+#on each occasion, the number multiplied by 2?
+
+numbers = [1, 2, 3, 4, 5]
+numbers.each {|x| puts x*2 }
