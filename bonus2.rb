28f411df5db4ee3a7e361389459eb856730b3608
Simon Cass <scass1991@gmail.com>
2018-07-24 19:25:32 +0100

    initial commit

---
+++ bonus2.rb
@@ -0,0 +1,11 @@
+#July 24th 2018
+#Write a method that takes an argument and returns the square of that number
+#return does not work on Atom, works when run using IRB however
+
+
+def numbersq(number)
+#switch this return statement for puts to get this to work in Atom IDE
+    return number ** 2
+end
+
+numbersq(4)
